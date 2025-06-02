d3cdc1c133e3d73e16d42aa0440f2da735176db9
erika-renau <erikarenaularrode@gmail.com>
2025-06-02 09:44:10 +0000

    Borrar comentarios

--- src/api/routes.py
+++ src/api/routes.py
@@ -13,12 +13,12 @@ def handle_hello():
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
     return jsonify(response_body), 200
-# :small_blue_diamond: Obtener todos los usuarios
+
 @api.route('/usuarios', methods=['GET'])
 def get_usuarios():
     usuarios = User.query.all()
     return jsonify([user.serialize() for user in usuarios]), 200
-# :small_blue_diamond: Crear un nuevo usuario
+
 @api.route('/usuarios', methods=['POST'])
 def create_usuario():
     data = request.get_json()
@@ -28,7 +28,7 @@ def create_usuario():
     nuevo_usuario = User(
         email=data["email"],
         user_name=data["user_name"],
-        password=data["password"],  # :warning: Recuerda hashearla en producción
+        password=data["password"],  
         ranking_user=data["ranking_user"],
         avatar=data["avatar"],
         experiencia=data["experiencia"]
