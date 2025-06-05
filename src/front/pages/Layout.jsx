0095c5e5aab6799705efc76f87ded170256131ff
Nelcy <nelgarpa22@gmail.com>
2025-06-05 10:26:55 +0200

    Homeprivate estable

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,15 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import ScrollToTop from "../components/ScrollToTop";
 import { CustomNavbar } from "../components/CustomNavbar";
 import { Footer } from "../components/Footer";
 
 export const Layout = () => {
+    const location = useLocation();
+    const isHome = location.pathname === "/";
+
     return (
         <ScrollToTop>
-            <CustomNavbar playerName="Rigo" />
-            <Outlet />
+            {!isHome && <CustomNavbar playerName="Rigo" />}
+            <main className="container-fluid px-0">
+                <Outlet />
+            </main>
             <Footer />
         </ScrollToTop>
     );
 };
-
