146a84bdf104d8c4697b8916aa8bd5f018005cab
fdezruizdavid <93644070+davidfr5@users.noreply.github.com>
2025-06-05 12:10:40 +0000

    Actualizacion necesaria (mucho)

--- src/api/models.py
+++ src/api/models.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     user_name: Mapped[str] = mapped_column(String(50), nullable=False)
