28a58a43ecddac6211b0c54ec22d99832cdcbccf
erika-renau <erikarenaularrode@gmail.com>
2025-06-18 10:47:44 +0000

    Efectos GameOverScreen

--- src/front/components/GameOverScreen.jsx
+++ src/front/components/GameOverScreen.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "./GameOverScreen.css";
+import Confetti from "react-confetti";
+import Lottie from "lottie-react";
+import gameOverAnimation from "../animations/gameOver.json";
 
 const GameOverScreen = ({
   score,
@@ -9,9 +12,42 @@ const GameOverScreen = ({
   onNewGame,
   onGoHome,
 }) => {
+  const isVictory = score >= 3;
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener ("resize", handleResize);
+  },[]);
+
   return (
-    <div className="game-over-screen d-flex flex-column align-items-center justify-content-center">
-      <h1 className="text-light mb-4">¡Juego Terminado!</h1>
+    <div className={`game-over-screen d-flex flex-column align-items-center justify-content-center ${
+      !isVictory ? "disaster-mode" : ""
+    }`}
+  >
+    {isVictory ? (
+    <Confetti width={windowSize.width} height={windowSize.height} />) : (
+    <Lottie
+      animationData={gameOverAnimation}
+      loop={true}
+      autoplay={true}
+      style={{ height: "200px", width: "200px" }}
+    />
+    )}
+
+      <h1 className="text-light mb-4">
+        {isVictory ? "¡Juego Terminado!" : "¡Desastre Total!"}
+      </h1>
+
       <p className="text-light mb-4 fs-4">
         Tu puntuación: <span className="score-final">{score}</span> de{" "}
         <span className="score-total">{totalQuestions}</span> preguntas
