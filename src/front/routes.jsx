7100833a7f52b79c904542bdb88c158d71bee8ab
Nelgarpa <nelgarpa22@gmail.com>
2025-06-20 12:38:05 +0000

    Rutas protegidas

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -7,24 +7,36 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import SalaDeJuego from "./pages/davidPages/SalaDeJuego";
 import { HomePrivate } from "./pages/home-private/HomePrivate";
-import {Registro } from "./pages/Registro";
-import {Login } from "./pages/Login";
-import  ForgotPass  from "./pages/ForgotPass";
-import  ResetPass  from "./pages/ResetPass";
+import { Registro } from "./pages/Registro";
+import { Login } from "./pages/Login";
+import ForgotPass from "./pages/ForgotPass";
+import ResetPass from "./pages/ResetPass";
+import { ProtectedRoute } from "../utils/ProtectedRoute";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-
     <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
-
       <Route path="/" element={<Home />} />
-      <Route path="/sala" element={<SalaDeJuego />} />
-      <Route path="/homeprivate" element={<HomePrivate />} />
+      <Route
+        path="/homeprivate"
+        element={
+          <ProtectedRoute>
+            <HomePrivate />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/sala"
+        element={
+          <ProtectedRoute>
+            <SalaDeJuego />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Registro />} />
-      <Route path="/recover" element={< ForgotPass />} />
-      <Route path="/reset" element={< ResetPass />} />
-      
+      <Route path="/recover" element={<ForgotPass />} />
+      <Route path="/reset" element={<ResetPass />} />
     </Route>
   )
 );
