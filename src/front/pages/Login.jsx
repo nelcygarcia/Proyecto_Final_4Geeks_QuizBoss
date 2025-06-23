15edc6641ab4e4a7fc63975c4f784d6b811b2105
erika-renau <erikarenaularrode@gmail.com>
2025-06-23 11:11:14 +0000

    Cambios en Login

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../../providers/AuthProvider";
 
 export const Login = () => {
-  const [email, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -15,7 +15,7 @@ export const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    if (!userOrEmail || !password) {
+    if (!email || !password) {
     setErrorMessage("Completa todos los campos");
     return;
   }
@@ -35,17 +35,7 @@ export const Login = () => {
       const data = await response.json();
 
       if (response.ok) {
-        dispatch({
-          type: "SET_AUTH",
-          payload: {
-            token: data.token,
-            email_id: data.email_id,
-          },
-        });
-
-        localStorage.setItem("token", data.token);
-        localStorage.setItem("user_id", data.email_id);
-
+        login(data.token, data.email_id);  // que dentro hace dispatch + localStorage
         navigate("/homeprivate");
       } else {
         if (response.status === 401 || response.status === 404) {
@@ -83,7 +73,7 @@ export const Login = () => {
             type="text"
             placeholder="Correo electrónico"
             value={email}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
           <input
